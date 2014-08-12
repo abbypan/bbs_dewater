5abdb9c6b6993b278ad6609c345c1cab7efcf0a8
Abby Pan <abbypan@gmail.com>
2014-08-12 13:35:55 +0800

    move to baidu cdn jquery

--- bbs_dewater.js
+++ bbs_dewater.js
@@ -3,7 +3,7 @@ var jq = jQuery || window.jQuery || $;
 if(! jq) {
     var script = document.createElement('script');
     script.type = "text/javascript";
-    script.src = "http://code.jquery.com/jquery-latest.min.js";
+    script.src = "http://libs.baidu.com/jquery/2.0.0/jquery.min.js";
     document.getElementsByTagName('head')[0].appendChild(script);
 }
 
